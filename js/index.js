fa0bd5e907ba2072da0d0513546fb21246aaacbf
Salvatore Dziekan <sdziekan@gmail.com>
2017-07-11 16:07:38 +0200

    taking a break and want to save my place

--- js/index.js
+++ js/index.js
@@ -50,10 +50,11 @@ MathGame.prototype.displayNumber = function(){
       $(".number-turn").text(this.cards[0]);
       $(".temp-cardtoremember").text(this.cards[1]);
       $(".temp-cardtoadd").text(this.cards[2]);
-      //
-      // setTimeout(function () {
-      //   $('div.hidden').fadeIn(1500).removeClass('hidden');
-      // }, 750);
+
+      setTimeout(function () {
+        $('div.hidden').fadeIn(1500).removeClass('hidden');
+      }, 750);
+
       this.carriageReturnPhase += 1;
       break;
 
@@ -62,6 +63,11 @@ MathGame.prototype.displayNumber = function(){
       $(".number-turn").text(this.cards[0]);
       $(".temp-cardtoremember").text(this.cards[1]);
       $(".temp-cardtoadd").text(this.cards[2]);
+
+      setTimeout(function () {
+        $('div.hidden').fadeIn(1500).removeClass('hidden');
+      }, 750);
+
       this.carriageReturnPhase += 1;
       break;
 
@@ -84,7 +90,7 @@ MathGame.prototype.displayNumber = function(){
         if (this.playerAnswer == this.correctAnswer ) {
           this.playerScore += 1;
         }
-        
+
         this.addNumber();
         this.removeNumber();
         this.correctAnswer = this.cards[0] + this.cards[2];
@@ -95,103 +101,45 @@ MathGame.prototype.displayNumber = function(){
         $(".temp-cardtoremember").text(this.cards[1]);
         $(".temp-cardtoadd").text(this.cards[2]);
         $(".temp-correctanswer").text(this.correctAnswer);
-
-
-
-
-
-
-
-
-
-  }
-
-//   if (this.carriageReturnPhase1 === true) {
-//     $(".number-turn").text(this.cards[0]);
-//     $(".temp-cardtoremember").text(this.cards[1]);
-//     $(".temp-cardtoadd").text(this.cards[2]);
-//
-//     setTimeout(function () {
-//       $('div.hidden').fadeIn(1500).removeClass('hidden');
-//     }, 750);
-//   } else if (this.)
-//
-//   // console.log("1cardVisible = " + this.card[0]);
-//   // console.log("1cardToRemember = " + this.cards[1]);
-//   // console.log("1cardToAdd = " + this.cards[2]);
+  };
 };
-//
-// //display 2nd card and set this.cardVisible value to this.cardToRemember
-// MathGame.prototype.displaySecondNumber = function () {
-//   var that = this;
-//   this.cards.unshift(this.generateNumber());
-//   $(".number-turn").text(this.cards[0]);
-//   $(".temp-cardtoremember").text(this.cards[1]);
-//   $(".temp-cardtoadd").text(this.cards[2]);
-//
-//   setTimeout(function () {
-//       $('div.hidden').fadeIn(1500).removeClass('hidden');
-//   }, 750);
-// }
-//
-// //display 3nd card and set initial value to cardToAdd
-// //calculated correctAnswer as well
-// MathGame.prototype.displayThirdNumber = function () {
-//   var that = this;
-//   this.cards.unshift(this.generateNumber());
-//   this.correctAnswer = this.cards[0] + this.cards[2];
-//   $(".number-turn").text(this.cards[0]);
-//   $(".temp-cardtoremember").text(this.cards[1]);
-//   $(".temp-cardtoadd").text(this.cards[2]);
-//   $(".temp-correctanswer").text(this.correctAnswer);
-//
-//
-//   console.log("3cardVisible = " + this.cards[0]);
-//   console.log("3cardToRemember = " + this.cards[1]);
-//   console.log("3cardToAdd = " + this.cards[2]);
-//   console.log("3correctAnswer = " + this.correctAnswer);
-// }
-//
-// //Script for carriage return in phase1 and phase2
-// // $(document).keydown(function(e) {
-// //   switch(e.which) {
-// //       case 13: // carriage return
-// //         that.displaySecondNumber();
-// //         break;
-// //
-// //       default: return; // exit this handler for other keys
-// //     };
-// //   });
-//
-//     // press "Enter"
-//     // this.cardToRemember = this.cardVisible
-// // MathGame.prototype.playerTurn = function ( {
-// //   var that = this;
-// // })
-//
-//     // hide this.cardVisible (fade out)
-//     //   this.cardVisible = selectNumber()
-//     // show this.cardVisible (fade in)
-//     // hide this.cardToAdd[0]
-//     // show this.cardToRemember
-//     // press "Enter"
-//     // generate this.cardVisible
-//     // hide this.cardToRemember
-//     // show this.cardVisible
-//     // generate correctAnswer = cardToAdd + cardVisible
-//     // reassign values this.cardToAdd = this.cardToRemember
-//     //   - this.cardToRemember = this.cardVisible
-//     //   - this.cardVisible = new random number
-//     // var playerAnswer = player types a number and "Enter"
-//     // if playerAnswer = correctAnswer, playerScore++
-//
-//
-//
-//
-//
-//
-//
-//
+
+
+//Script for carriage return in phase1 and phase2
+$(document).keydown(function(e) {
+  switch(e.which) {
+      case 13: // carriage return
+        that.displaySecondNumber();
+        break;
+
+      default: return; // exit this handler for other keys
+    };
+  });
+
+
+    // hide this.cardVisible (fade out)
+    //   this.cardVisible = selectNumber()
+    // show this.cardVisible (fade in)
+    // hide this.cardToAdd[0]
+    // show this.cardToRemember
+    // press "Enter"
+    // generate this.cardVisible
+    // hide this.cardToRemember
+    // show this.cardVisible
+    // generate correctAnswer = cardToAdd + cardVisible
+    // reassign values this.cardToAdd = this.cardToRemember
+    //   - this.cardToRemember = this.cardVisible
+    //   - this.cardVisible = new random number
+    // var playerAnswer = player types a number and "Enter"
+    // if playerAnswer = correctAnswer, playerScore++
+
+
+
+
+
+
+
+
 
 var g = new MathGame();
-//console.log(g.generateNumber());
+console.log(g.generateNumber());
